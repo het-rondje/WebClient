3fc3d23f26f71df865c34af783088c120748236f
Jan-Paul <jpmosterdijk@gmail.com>
2019-06-13 14:42:30 +0200

    Revert "Merge branch 'development' of https://github.com/het-rondje/WebClient into development"
    This reverts commit cd623a55ec05687004f74e36c8378eed345c7c01, reversing
    changes made to 758e681c80b3f7ec05eb6ba00bdbd9da6b2cf38e.

--- Front-end/src/app/services/authentication.service.ts
+++ Front-end/src/app/services/authentication.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+
 //Hash package importeren
 
 @Injectable({
@@ -7,33 +13,98 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
 
   private date: number;
-  private message: string;
+  private message: string = "";
   private hashedMessage: string;
 
-  constructor() { }
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) { 
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
 
   login(privateKey: String) {
     //Set date to now:
     this.date = Date.now();
 
+    //Object Back-end wants when sending a chat message:
+    var a: { id: string, message: string, signature: string };
+
     //Create message:
     this.message = this.date + "-login";
+    //Add userID
 
     //Hash message + encrypt hash:
-    //this.digitalSignature(privateKey, this.message);
+    this.digitalSignature(privateKey, this.message);
+
+
+    return this.http.post<any>(`${environment.apiUrl}/login`, { /* digital signature here */ })
+    .pipe(map(user => {
+        // login successful if there's a jwt token in the response
+        console.log('token -> ' + user.token);
+        if (user && user.token) {
+            console.log("valid");
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+        }
+        return user;
+    }));
   }
 
-  // digitalSignature(privateKey: String, message: string) {
-  //   //Hashing message:
-  //   var hash = require('hash.js');
-  //   this.hashedMessage = hash.sha256().update(message).digest('hex');
-  //   console.log(message);
-  //   console.log(this.hashedMessage);
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+}
 
-  //   //Hash encrypten:
+  digitalSignature(privateKey: String, message: string) {
+    /*
+    //Hashing message:
+    var hash = require('hash.js');
+    this.hashedMessage = hash.sha256().update(message).digest('hex');
+    console.log('Message to encrypt: ' + message);
+    console.log('Hash from message: ' + this.hashedMessage);
+
+    //Hash encrypten:
+
+    // //Jan's piece
+    // const fs = require('fs')
+    // const crypto = require('crypto')
+
+    // // Get key //Might have to get it from a parameter
+    // const pem = fs.readFileSync('../private.pem')
+    // const key = pem.toString('ascii')
+
+    // //Create Signature
+    // const sign = crypto.createSign('RSA-SHA256')
+    // sign.update(this.hashedMessage)
+    // // const sig = sign.sign(key,'hex')
+    // const sig = sign.sign(privateKey,'hex')
+    // console.log('Signature: ' + sig)
+
+    const NodeRSA = require('node-rsa');
+    //Get Key
+    const key = new NodeRSA(privateKey);
     
+    const text = 'Hello RSA!';
 
-  //   //return encrypted hash
-  // }
+    //Enrypt
+    const encrypted = key.encrypt(text, 'hex');
+    console.log('encrypted: ', encrypted);
+
+    //return encrypted hash
+    return message;*/
+  }
+
+
+  //return encrypted hash
+
 
 }

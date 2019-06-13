b221eee50d289195cb46d77f8b13fab3a27e9e3e
Jan-Paul <jpmosterdijk@gmail.com>
2019-06-13 11:52:14 +0200

    login base + urls in environment

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,7 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: environment.socketUrl, options: {} };
 
 
 @NgModule({
--- Front-end/src/app/services/authentication.service.ts
+++ Front-end/src/app/services/authentication.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+
 //Hash package importeren
 
 @Injectable({
@@ -10,7 +16,18 @@ export class AuthenticationService {
   private message: string;
   private hashedMessage: string;
 
-  constructor() { }
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient) { 
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
 
   login(privateKey: String) {
     //Set date to now:
@@ -21,7 +38,27 @@ export class AuthenticationService {
 
     //Hash message + encrypt hash:
     this.digitalSignature(privateKey, this.message);
+
+
+    return this.http.post<any>(`${environment.apiUrl}/login`, { /* digital signature here */ })
+    .pipe(map(user => {
+        // login successful if there's a jwt token in the response
+        console.log('token -> ' + user.token);
+        if (user && user.token) {
+            console.log("valid");
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+        }
+        return user;
+    }));
   }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+}
 
   digitalSignature(privateKey: String, message: string) {
     //Hashing message:
@@ -34,6 +71,7 @@ export class AuthenticationService {
     
 
     //return encrypted hash
+    return message;
   }
 
 }

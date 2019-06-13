4d8e63d9be63ce87c8ad74add0e406f6e4cee200
KGerretsen <33416856+KGerretsen@users.noreply.github.com>
2019-06-13 12:26:27 +0200

    digital signature, not working yet

--- Front-end/src/app/services/authentication.service.ts
+++ Front-end/src/app/services/authentication.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 //Hash package importeren
 
 @Injectable({
@@ -7,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
 
   private date: number;
-  private message: string;
+  private message: string = "";
   private hashedMessage: string;
 
   constructor() { }
@@ -16,24 +17,57 @@ export class AuthenticationService {
     //Set date to now:
     this.date = Date.now();
 
+    //Object Back-end wants when sending a chat message:
+    var a: { id: string, message: string, signature: string };
+
     //Create message:
     this.message = this.date + "-login";
+    //Add userID
 
     //Hash message + encrypt hash:
     this.digitalSignature(privateKey, this.message);
+
+    //return ...
   }
 
   digitalSignature(privateKey: String, message: string) {
     //Hashing message:
     var hash = require('hash.js');
     this.hashedMessage = hash.sha256().update(message).digest('hex');
-    console.log(message);
-    console.log(this.hashedMessage);
+    console.log('Message to encrypt: ' + message);
+    console.log('Hash from message: ' + this.hashedMessage);
 
     //Hash encrypten:
-    
 
-    //return encrypted hash
+    // //Jan's piece
+    // const fs = require('fs')
+    // const crypto = require('crypto')
+
+    // // Get key //Might have to get it from a parameter
+    // const pem = fs.readFileSync('../private.pem')
+    // const key = pem.toString('ascii')
+
+    // //Create Signature
+    // const sign = crypto.createSign('RSA-SHA256')
+    // sign.update(this.hashedMessage)
+    // // const sig = sign.sign(key,'hex')
+    // const sig = sign.sign(privateKey,'hex')
+    // console.log('Signature: ' + sig)
+
+    const NodeRSA = require('node-rsa');
+    //Get Key
+    const key = new NodeRSA(privateKey);
+    
+    const text = 'Hello RSA!';
+
+    //Enrypt
+    const encrypted = key.encrypt(text, 'hex');
+    console.log('encrypted: ', encrypted);
+
   }
+
+
+  //return encrypted hash
+
 
 }

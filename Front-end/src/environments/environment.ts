36fdcdc48bf8b017e3512609a4345a7fb53e87d0
Jan-Paul <jpmosterdijk@gmail.com>
2019-06-14 13:52:09 +0200

    online shit

--- Front-end/src/environments/environment.ts
+++ Front-end/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3001/api",
-  socketUrl: "http://localhost:3000"
+  apiUrl: "159.65.197.36:3001/api",
+  socketUrl: "159.65.197.36:3000"
 
 };
 

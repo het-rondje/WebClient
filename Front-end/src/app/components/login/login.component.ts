1755b954940b246d903ec7e84ea1260c72a42b05
KGerretsen <33416856+KGerretsen@users.noreply.github.com>
2019-06-17 13:46:46 +0200

    Added login function with signature

--- Front-end/src/app/components/login/login.component.ts
+++ Front-end/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,22 +9,23 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class LoginComponent implements OnInit {
 
-  private privateKey: String;
-  constructor(private auth: AuthenticationService) { }
+  private hardcodedKey: string = "-----BEGIN PRIVATE KEY-----MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCnIlcAVr2KMii8n0ykvIcizGP/NBHIOhcYFwjoupt1QbrWgmuZ7TtAyeiKvG55widDno2uU7DZAMlnk8IXRrKg6uOqNPg5sR3hozllmg7fusVKGaRR5WtJ0ScuehtgXDjkqCQkgxLgJOMrCzxHQCXRJTBUp1Zvb94bCR00A9k7CP7jT0sh79yWHJPv50qgaATA7wuDmFs700FBxDj4iDB+jvr1Lh+N0dcrKLoBuMKS/yikoXHwBmkCUKbDPfIscZ2gc0wKaNyKFTSFugoXncU962bR3dp/NZsWj9+8UCTT7hYU/crID/szFm3otsqL5aJJ56l/NHPypZXzNDkijfPDAgMBAAECggEBAJnmqS6KsSZcYd6u4aigYd/NThdmHym9NB7etzl2/EfRKvb0SZkINpPGWPfH5AMCoZ1Glzh7oFFyxAM4sHU2iK5dHR9Sf3DAg7+Oyf+hCSlDlwBQ9FrlYxIqImZN0n+87DbCDeYBPJRh4z1zryH9E9Wu5P5OyOz8mg1TKy0GjjYo4tehLGNslTyV35+RnR9CdCRng9nT0rVMLr+C5Z578Yz61Q5s5RuLlC+qC8FqXipU4rf0B/sMhbdWQ6diKqpxbchLvNEwA19RqMMJdsxZtjpwzUXFiWjxtz7BjY33XK2SYEO8h79oF6KzdxEOdM9p3g/02snqJTpjWpMkdxAwTqECgYEA+ToM2mkdMC7/2VoHaUfmQsHMzUDNNLp/bsnqqJzzvKlTusjJ8SJINVZ7UZ6pk+NG2hnfpTH5mJZjELZ9Sog1w++h3RpSRG4K5hEnwhN02kFVcOcZEfVPTAV5lrVZEGRWiHSeVHEesjaTB+ICNnJgJV0UXvRAS6fDZlQQ0ZXzp0cCgYEAq60lK4Xc+GpkD/9cY+3/u42z1GhCxjj6wW7vPplNwzk07gqvIc+QA2sbwTfQsKkFggUngfvytrcdTh1J9AevLr7Gyh8Nh9+uCGP3jpiynw2XkuV6ycz+7dHXzo1q7YYUNg+yvp/oG/3f4o3RLddRInoBe+z1WUtoNtKSuUyrRaUCgYEA3DAyXODTXdeYSUJmG9jfEYnBVHH1GqnOnmYcQP3xxXPgOrzc1YLRQvZ2G0VPUw5Qca68TWnat7neKkzgYvH2dnaHjh2MI1MJo/vyIWApNA7W2toB50DQ3zxTERiFaJsNbJ+Dpt0OHqtiuWsvQ0VHtpsGvWGUUIMWepi5B0IxM08CgYEAndkLjiL886PO8S6YYoFYW7iEmk5q5xtI5EX90A7hAxk8Y4qLUIkqHxqCYHvTdDtUhyH6G3fuyLVBSqt/bdMK7e4XlYxLwpyVxb00x3X69mdm8HfzGE1WMJB63r4EjPR07OguLzuUDPi2u/YtWPra7NpFNw6+4CWHYrDmm121NMkCgYBA3zsb+pb2yZnWmyK7nQOft9cr384kpjyQmmzqNuWKS7EJCdzjhOrVJ+DF8nQRQ4disjqxJfQCILhXlvfkFynZN4sZnBvP1/XU2oX/hrHvNdlUcBpJbI568ke/AYZrDzMgbqb5jK1S1ButlRgd541xDSe0k4dxVHhcFdhUZlokIw==-----END PRIVATE KEY-----";
+  private hardcodedId: string = "Io9px3dQP";
+
+  private privateKey: string;
+  private id: string;
+  constructor(private auth: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
   }
 
-  loginUser(){
-  //Naar de service de key sturen.
-  this.auth.login(this.privateKey);
-  //Wat willen we terug?
-  //token? var logged in true zetten?
 
-  }
-
-  test(){
-    this.auth.login("");
+  loginUser() {
+    this.auth.login(this.privateKey, this.id).subscribe(response => {
+      console.log(response)
+      if (this.auth.isLoggedIn())
+        this.router.navigateByUrl('/')
+    })
   }
 
 }
--- Front-end/src/app/services/authentication.service.ts
+++ Front-end/src/app/services/authentication.service.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
 import * as hash from 'hash.js';
 import * as crypto from 'crypto-js';
+import { Router } from '@angular/router';
 
 //Hash package importeren
 
@@ -14,18 +15,16 @@ import * as crypto from 'crypto-js';
 })
 export class AuthenticationService {
 
-  private randomString: string = "";
+  private loggedIn: boolean = false;
+
   private hashedMessage: string;
-  private userId: string;
   public signature: string;
   private httpOptions = {};
-  private encryptSecretKey = "-----BEGIN RSA PRIVATE KEY----- MIICXgIBAAKBgQDHikastc8+I81zCg/qWW8dMr8mqvXQ3qbPAmu0RjxoZVI47tvskYlFAXOf0sPrhO2nUuooJngnHV0639iTTEYG1vckNaW2R6U5QTdQ5Rq5u+uV3pMk7w7Vs4n3urQ6jnqt2rTXbC1DNa/PFeAZatbf7ffBBy0IGO0zc128IshYcwIDAQABAoGBALTNl2JxTvq4SDW/3VH0fZkQXWH1MM10oeMbB2qO5beWb11FGaOO77nGKfWcbYgfp5Ogrql4yhBvLAXnxH8bcqqwORtFhlyV68U1y4R+8WxDNh0aevxH8hRS/1X5031DJm1JlU0E+vStiktN0tC3ebH5hE+1OxbIHSZ+WOWLYX7JAkEA5uigRgKp8ScGauUijvdOLZIhHWq7y5Wz+nOHUuDw8P7wOTKU34QJAoWEe771p9Pf/GTA/kr0BQnPQvWUDxGzJwJBAN05C6krwPeryFKrKtjOGJIniIoY72wRnoNcdEEs3HDRhf48YWForiRbZylzzzNFy/gmzT6XJQTfktGqq+FZD9UCQGIJaGrxHJgfmpDuAhMzGsUsYtTriRox0D1Iqa7dhE693t5aBG010OF6MLqdZA1CXrn5SRtuVVaCSLZEL/2J5UcCQQDAd3MXucNnN4NPuS/L9HMYJWD7lPoosaORcgyK77bSSNgk+u9WSjbH1uYIAIPSffUZbti+jc1dUg5wb+aeZlgJAkEAurrpmpqj5vg087ZngKfFGR5rozDiTsK5DceTV97Ka3Y+Nzl+XWTxDBWk4YPh2ZlKv402hZEfWBYxUDn5ZkH/bw== -----END RSA PRIVATE KEY-----";
-
 
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
     this.currentUser = this.currentUserSubject.asObservable();
   }
@@ -34,70 +33,53 @@ export class AuthenticationService {
     return this.currentUserSubject.value;
   }
 
-  login(privateKey: String) {
+  login(privateKey: string, id: string) {
+    let body = {}
 
-    // const sig = this.digitalSignature(privateKey, this.generateRandomString(15));
-    const sig = this.digitalSignature(privateKey, '');
-    let timestamp = Date.now.toString();
+    let date = new Date;
+    let timestamp = date.getTime().toString(); //FORMAT??
+
     const timeSignature = this.digitalSignature(privateKey, timestamp);
-
-    let body = { 'randomString': this.randomString }
 
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'signature': sig,
+        // 'signature': sig, // body signature NOT FOR LOGIN
         'timestamp': timestamp,
-        'timeSignature': timeSignature,
-        'id': this.userId
+        'timesignature': timeSignature,
+        'userid': id,
+        'id': id
       })
     };
 
-    return this.http.post('${environment.apiUrl}/users' + this.userId, body, this.httpOptions)
+    return this.http.post('http://localhost:3000/api/users/' + id, body, this.httpOptions)
       .pipe(
         map(result => {
-          let user: User;
+          console.log(result)
+
+          let user: User =new User;
           let json = JSON.stringify(result);
           let obj = JSON.parse(json)
-          user._id = obj._id;
-          user.firstname = obj.user.firstname;
-          user.lastname = obj.user.lastname;
-          //not in response..
-          //user.messages = obj.messages;
+          user._id = obj.user.id;
+          user.firstname = obj.user.firstName;
+          user.lastname = obj.user.lastName;
 
-          //localStorage.setItem('currentUser', JSON.stringify(user));
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          console.log(user)
+          this.loggedIn = true;
           return user;
         })
       )
   }
 
 
-  private digitalSignature(privateKey: String, content: any) {
-    //Generate random string:
-    //this.randomString = this.generateRandomString(15);
-
-    //Hashing message:
+  private digitalSignature(privateKey: string, content: any) {
+    //Hashing content:
     this.hashedMessage = this.hash(content);
 
-    console.log('Message to encrypt: ' + content);
-    console.log('Hash from message: ' + this.hashedMessage);
-    console.log('Encrypted: ' + this.encryptData(this.hashedMessage))
-
     //Return signature
-    //this.signature = this.encryptData(this.hashedMessage);
-    return this.encryptData(this.hashedMessage);
+    return this.encryptData(this.hashedMessage, privateKey);
   }
-
-  //Generate a random string:
-  // private generateRandomString(length: number) {
-  //   var result = '';
-  //   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  //   var charactersLength = characters.length;
-  //   for (var i = 0; i < length; i++) {
-  //     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  //   }
-  //   return result;
-  // }
 
   //Hashes a given string message with sha256
   private hash(message: string) {
@@ -105,9 +87,9 @@ export class AuthenticationService {
   }
 
   //Encrypts given data with this.encryptSecretKey
-  private encryptData(data) {
+  private encryptData(data, privateKey: string) {
     try {
-      return crypto.AES.encrypt(JSON.stringify(data), this.encryptSecretKey).toString();
+      return crypto.AES.encrypt(JSON.stringify(data), privateKey).toString();
     } catch (e) {
       console.log(e);
     }
@@ -116,6 +98,13 @@ export class AuthenticationService {
   logout() {
     // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
+    this.loggedIn = false;
     this.currentUserSubject.next(null);
+    this.router.navigate(['/login'])
   }
+
+  isLoggedIn(){
+    return this.loggedIn;
+  }
+
 }

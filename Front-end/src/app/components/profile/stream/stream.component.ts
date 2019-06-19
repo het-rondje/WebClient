302591a17c962b54e141673d66fc3e8a17f6eea3
Jan-Paul <jpmosterdijk@gmail.com>
2019-06-19 12:06:04 +0200

    use the stream id of the current profile

--- Front-end/src/app/components/profile/stream/stream.component.ts
+++ Front-end/src/app/components/profile/stream/stream.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as flvjs from '../../../../../node_modules/flv.js/dist/flv';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-stream',
@@ -8,7 +9,7 @@ import * as flvjs from '../../../../../node_modules/flv.js/dist/flv';
 })
 export class StreamComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
     //if (flvjs.isSupported()) {
@@ -18,7 +19,7 @@ export class StreamComponent implements OnInit {
           type: 'flv',
           "isLive": true,
           //url: 'http://localhost:8000/live/mykey.flv'
-          url: 'http://159.65.197.36:8000/live/mykey.flv'
+          url: 'http://159.65.197.36:8000/live/' + this.userService.getSelectedUser()._id + '.flv'
 
       });
       flvPlayer.attachMediaElement(videoElement);
